5fc69206417baf6f7701fa4691b462d848f63ff3
Geoff Williams <geoff.williams@puppetlabs.com>
2015-08-05 20:05:24 +1000

    make cert generation optional

--- manifests/register.pp
+++ manifests/register.pp
@@ -1,20 +1,30 @@
 # Generate and sign a new mcollective user certificate, then copy it to
 # MCollective's public key directory
+#
+# Params
+# [*title*]
+#   cert_name (unique id) or arbiratry string if both specified    
+# [*cert_name*]
+#   unique id of user to register
+# [*generate_cert*]
+#   generate a new certifcate (on the puppet master)?
 define mcollective_user::register(
-    $cert_name = $title
+    $cert_name     = $title,
+    $generate_cert = true,
 ) { 
 
-
-  # generata and accept a certificate for r10k user
-  exec { "r10k_mco_cert":
-    command  => "puppet certificate generate ${cert_name} --ca-location local && puppet cert sign ${cert_name}",
-    creates  => "${::settings::ssldir}/ca/signed/${cert_name}.pem",
-    path     => [
-      "/opt/puppetlabs/puppet/bin",
-      "/opt/puppet/bin",
-      "/usr/bin",
-      "/bin",
-    ],
+  if $generate_cert {
+    # generata and accept a certificate for r10k user
+    exec { "r10k_mco_cert":
+      command  => "puppet certificate generate ${cert_name} --ca-location local && puppet cert sign ${cert_name}",
+      creates  => "${::settings::ssldir}/ca/signed/${cert_name}.pem",
+      path     => [
+        "/opt/puppetlabs/puppet/bin",
+        "/opt/puppet/bin",
+        "/usr/bin",
+        "/bin",
+      ],
+    }
   }
 
   # Once generated, copy the private key into MCollective's known public keys dir
